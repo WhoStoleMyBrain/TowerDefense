e3da78a854f90044feb2f0bacdd5ea4647653b29
MW <marcowetter@gmx.de>
2022-02-01 21:50:05 +0100

    changed infrastructure to allow better scaling and seperation of concerns

--- main.py
+++ main.py
@@ -1,78 +1,197 @@
 import pygame
 import os
-import sys
-
-from settings import GameDefaults, Window
-from eventHandler import EventHandler
 
 
-class Game:
+class Core:
+    """
+        The core class is the first entry point to the whole game.
+        It handles the game settings, game data, windows and more. Everything comes together at the core.
+    """
+
     def __init__(self):
         pygame.init()
         pygame.font.init()
         pygame.mixer.init()
-        self.gameDefaults = GameDefaults()
+        self.Settings = Settings()
         self.playing = False
         self.clock = pygame.time.Clock()
-        self.mainWindow = Window()
-        self.squares = [(4, 8)]
+        self.Window = Window(self.Settings)
         self.Towers = pygame.sprite.Group()
 
-    def mainFrame(self):
+    def setWindow(self, window):
+        self.Window = window(self.Settings)
+
+
+class Settings:
+    """
+        The settings saves the settings of the game. Resolution, window size and others are important
+    """
+    def __init__(self):
+        self.TITLE = 'Tower Defense'
+        self.FPS = 60
+        self.WIDTH = 1920
+        self.HEIGHT = 1080
+        self.COLORS = {}
+        self.setColors()
+        self.BGCOLOR = self.COLORS['GREY']
+
+    def setColors(self):
+        self.COLORS['WHITE'] = (255, 255, 255)
+        self.COLORS['GREY'] = (220, 220, 220)
+        self.COLORS['BLACK'] = (0, 0, 0)
+        self.COLORS['RED'] = (255, 0, 0)
+        self.COLORS['YELLOW'] = (255, 255, 0)
+        self.COLORS['GREY'] = (30, 30, 30)
+
+
+class EventHandler:
+    def __init__(self):
         pass
 
+    def handleEvents(self, events, core):
+        for event in events:
+            if event.type == pygame.QUIT:
+                core.playing = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = pygame.mouse.get_pos()
+                x_grid, y_grid = core.Window.GameData.transformKoordinatesToSquares(x, y)
+                x_snapped, y_snapped = core.Window.GameData.transformSquaresToCoordinates((x_grid, y_grid))
+                arrow_tower = ArrowTower(os.path.join('Assets', 'axeDouble2.png'),
+                                         os.path.join('Assets', 'Grenade+1.mp3'), (x_snapped, y_snapped),
+                                         (core.Window.GameData.TILESIZE - core.Window.GameData.GRIDWIDTH,
+                                          core.Window.GameData.TILESIZE - core.Window.GameData.GRIDWIDTH))
+                arrow_tower.place(core.Window.GameData.Towers)
+
+
+class Window:
+    """
+        The Window class holds all elements relevant for displaying any window.
+        This can be the main window, but also be the game window
+    """
+    def __init__(self, settings):
+        self.Settings = settings
+        self.WIN = pygame.display.set_mode((self.Settings.WIDTH, self.Settings.HEIGHT))
+        self.GameData = GameData(self.Settings)
+        pygame.display.set_caption(self.Settings.TITLE)
+
     def drawWindow(self):
-        self.mainWindow.WIN.fill(self.gameDefaults.BGCOLOR)#WIN.blit(self.mainWindow.SPACE, (0, 0))
+        self.WIN.fill(self.Settings.BGCOLOR)#WIN.blit(self.Window.SPACE, (0, 0))
         self.drawGrid()
-        self.Towers.draw(self.mainWindow.WIN)
-        self.Towers.update()
+        self.GameData.Draw(self.WIN)
         pygame.display.flip()  # Can use pygame.display.update(), which can update single elements of the display
 
     def drawGrid(self):
-        for grid_x in range(self.gameDefaults.TILES_X + 1):
-            x = grid_x * self.gameDefaults.TILESIZE + self.gameDefaults.MARGIN
-            self.drawLine((x, self.gameDefaults.MARGIN), (x, self.gameDefaults.HEIGHT - self.gameDefaults.MARGIN))
+        for grid_x in range(self.GameData.TILES_X + 1):
+            x = grid_x * self.GameData.TILESIZE + self.GameData.MARGIN
+            self.drawLine((x, self.GameData.MARGIN), (x, self.Settings.HEIGHT - self.GameData.MARGIN))
 
-        for grid_y in range(self.gameDefaults.TILES_Y + 1):
-            y = grid_y * self.gameDefaults.TILESIZE + self.gameDefaults.MARGIN
-            self.drawLine((self.gameDefaults.MARGIN, y), (self.gameDefaults.WIDTH - self.gameDefaults.MARGIN, y))
+        for grid_y in range(self.GameData.TILES_Y + 1):
+            y = grid_y * self.GameData.TILESIZE + self.GameData.MARGIN
+            self.drawLine((self.GameData.MARGIN, y), (self.Settings.WIDTH - self.GameData.MARGIN, y))
 
     def drawLine(self, point1, point2):
-        pygame.draw.line(self.mainWindow.WIN, self.gameDefaults.GREY, point1, point2,
-                         width=self.gameDefaults.GRIDWIDTH)
+        pygame.draw.line(self.WIN, self.Settings.COLORS['WHITE'], point1, point2,
+                         width=self.GameData.GRIDWIDTH)
+
+
+class GameData:
+    """
+        In GameData the relevant Data for the game are stored.
+        While Towers, Enemies and Projectiles are part of this class,
+        other information such as the Players Life, Money and more is saved here.
+    """
+    def __init__(self, settings):
+        self.TILESIZE = 32
+        self.Settings = settings
+        self.MARGIN = 5
+        self.TILES_X = (self.Settings.WIDTH - 2 * self.MARGIN) // self.TILESIZE #  59
+        self.TILES_Y = (self.Settings.HEIGHT - 2 * self.MARGIN) // self.TILESIZE
+        self.GRIDWIDTH = 2
+        self.Towers = pygame.sprite.Group()
 
     def transformKoordinatesToSquares(self, x, y):
-        square_x = (x - self.gameDefaults.MARGIN) // self.gameDefaults.TILESIZE
-        square_y = (y - self.gameDefaults.MARGIN) // self.gameDefaults.TILESIZE
+        square_x = (x - self.MARGIN) // self.TILESIZE
+        square_y = (y - self.MARGIN) // self.TILESIZE
         return square_x, square_y
 
     def transformSquaresToCoordinates(self, position_square):
-        x = position_square[0] * self.gameDefaults.TILESIZE + self.gameDefaults.MARGIN + self.gameDefaults.GRIDWIDTH
-        y = position_square[1] * self.gameDefaults.TILESIZE + self.gameDefaults.MARGIN + self.gameDefaults.GRIDWIDTH
+        x = position_square[0] * self.TILESIZE + self.MARGIN + self.GRIDWIDTH
+        y = position_square[1] * self.TILESIZE + self.MARGIN + self.GRIDWIDTH
         return x, y
 
-    def fillSquare(self, x, y, color=(255, 255, 0)):
-        left = x * self.gameDefaults.TILESIZE + self.gameDefaults.MARGIN + self.gameDefaults.GRIDWIDTH
-        top = y * self.gameDefaults.TILESIZE + self.gameDefaults.MARGIN + self.gameDefaults.GRIDWIDTH
-        rect = pygame.Rect(left, top, self.gameDefaults.TILESIZE - self.gameDefaults.GRIDWIDTH,
-                           self.gameDefaults.TILESIZE - self.gameDefaults.GRIDWIDTH)
-        pygame.draw.rect(self.mainWindow.WIN, color, rect)
+    def Draw(self, WIN):
+        self.Towers.update()
+        self.Towers.draw(WIN)
+
+
+class Sound:
+    def __init__(self):
+        pass
+
+
+class LevelSettings:
+    """
+        The settings required for the different levels are saved here.
+        To be more precise, the settings should be loaded from a file.
+        Saving to a file would also enable a custom world editor.
+    """
+    def __init__(self):
+        pass
+
+
+class Tower:
+    def __init__(self):
+        pass
+
+
+class ArrowTower(pygame.sprite.Sprite):
+    def __init__(self, picture_path, sound_path, position, size):
+        super().__init__()
+        self.image = pygame.transform.scale(pygame.image.load(picture_path), size)
+        self.position = position
+        self.rect = self.image.get_rect()
+        self.rect.topleft = self.position
+        self.sound = pygame.mixer.Sound(sound_path)
+
+    def place(self, tower_group):
+        collided_sprites = pygame.sprite.spritecollide(self, tower_group, False)
+        if len(collided_sprites) <= 0:
+            tower_group.add(self)
+        else:
+            tower_group.remove(collided_sprites[0])
+
+    def update(self):
+        self.rect.topleft = self.position
+
+
+class Enemy:
+    def __init__(self):
+        pass
+
+
+class Projectile:
+    def __init__(self):
+        pass
+
+
+class Game:
+    def __init__(self):
+        pass
 
 
 def main():
-    game = Game()
+    core = Core()
     clock = pygame.time.Clock()
     eventhandler = EventHandler()
-    game.playing = True
-    while game.playing:
-        game.mainFrame()
-        clock.tick(game.gameDefaults.FPS)
+    core.playing = True
+    while core.playing:
+        clock.tick(core.Settings.FPS)
 
-        eventhandler.handle_events(pygame.event.get(), game)
-        if not game.playing:
+        eventhandler.handleEvents(pygame.event.get(), core)
+        if not core.playing:
             pygame.quit()
             break
-        game.drawWindow()
+        core.Window.drawWindow()
 
 
 if __name__ == '__main__':
